182dcef372ee87699778cf24a3558bac63abdb9f
Jonas Daniel Lima <jdlima@PARFOIS.com>
2024-03-11 15:11:29 +0000

    script

--- BusinessLayer/Models/TaskModel.cs
+++ BusinessLayer/Models/TaskModel.cs
@@ -8,5 +8,11 @@ namespace BusinessLayer.Model
 {
     public class TaskModel
     {
+        public int TaskID { get; set; }
+        public string Department { get; set; }
+        public string TaskCode { get; set; }
+        public int WorkerID { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
     }
 }
